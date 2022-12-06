ed9d4bc8e444eb06adc3c900cba5ac0c95888996
Guigghisi <guilhermeghisi14@gmail.com>
2022-12-06 11:03:22 -0300

    M2S08E2

---
+++ Hello/src/main/java/controller/hello.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import model.PessoaModel;
+import service.PessoaService;
+
+
+@WebServlet("/hello")
+public class hello extends HttpServlet {
+	
+	private PessoaService service = new PessoaService();
+	private Gson gson = new Gson();
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String json = gson.toJson(this.service.getPessoas());
+		response.getWriter().append(json);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BufferedReader reader = request.getReader();
+		PessoaModel pessoa = gson.fromJson(reader, PessoaModel.class);
+		service.addPessoa(pessoa);
+		doGet(request, response);
+	}
+
+}
